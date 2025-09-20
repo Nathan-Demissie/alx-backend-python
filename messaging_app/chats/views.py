60bc4a431bd5505fc599dab339f99a18c5eb0113
Avstel <it.nathan.demissie@gmail.com>
2025-09-20 15:13:59 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,1 +1,20 @@
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and creating conversations.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and sending messages to conversations.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
 
