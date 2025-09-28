668ec81db038e09dfde161c450c0188b3ca76326
Avstel <it.nathan.demissie@gmail.com>
2025-09-28 11:32:48 +0300

    Create wsgi.py

---
+++ Django-Middleware-0x03/Django_Middleware_0x03/wsgi.py
@@ -0,0 +1,5 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Middleware_0x03.settings')
+application = get_wsgi_application()
