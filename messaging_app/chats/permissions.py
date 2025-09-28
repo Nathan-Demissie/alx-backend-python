3a455bf93ad9c2786278fdd30dc7adb70f29bbb7
Avstel <it.nathan.demissie@gmail.com>
2025-09-28 10:41:56 +0300

    Create permissions.py

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrParticipant(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # For messages
+        if hasattr(obj, 'sender'):
+            return obj.sender == request.user or request.user in obj.conversation.participants.all()
+        # For conversations
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+        return False
