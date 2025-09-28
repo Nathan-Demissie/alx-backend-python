77f517ddab73b4b6c9754306859318eb87b8cb5f
Avstel <it.nathan.demissie@gmail.com>
2025-09-28 10:43:20 +0300

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -7,3 +7,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
